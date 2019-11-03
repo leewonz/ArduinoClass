902c1a3bb24e10243ce7e047f4c5c8a79dd947a4
leewonz <lwk530@gmail.com>
2019-11-03 12:56:50 +0900

    Update LCD.ino

--- LCD/LCD.ino
+++ LCD/LCD.ino
@@ -5,43 +5,35 @@
 #include <LiquidCrystal_I2C.h> // LCD 1602 I2C용 라이브러리
 
 
-LiquidCrystal_I2C lcd(0x3F,16,2); // 접근주소: 0x3F or 0x27
+LiquidCrystal_I2C lcd(0x27,16,2); // 접근주소: 0x3F or 0x27
 
+String line1 = "Hello, world!";
+String line2 = "Hello, world!";
 
 void setup()
 
 
 {
-
-
-lcd.init(); // LCD 초기화
-
-
-// Print a message to the LCD.
-
-
-lcd.backlight(); // 백라이트 켜기
-
-
-lcd.setCursor(0,0); // 1번째, 1라인
-
-
-lcd.print("Hello, world!");
-
-
-lcd.setCursor(0,1); // 1번째, 2라인
-
-
-lcd.print("Enjoy - Eduino");
-
+  lcd.init(); // LCD 초기화
+  
+  lcd.backlight(); // 백라이트 켜기
 
 }
 
 
 void loop()
-
-
 {
+  line1.concat(line1.charAt(0));
+  line1.remove(0, 1);
 
+  line2.concat(line2.charAt(0));
+  line2.remove(0, 1);
+    
+  lcd.setCursor(0,0); // 1번째, 1라인
+  lcd.print(line1);
+  
+  lcd.setCursor(0,1); // 1번째, 2라인
+  lcd.print(line2);
 
+  delay(100);
 }
